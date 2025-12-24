059d16b56247f9bb6355c5018456a352aa374a42
Shameer Sirajuddin <shameer707@gmail.com>
2025-12-24 19:15:51 +0530

    0.0.1 Deplyoment test

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from Docker, uv, and FastAPI!"}
+    return {"message": "Hello from Docker, uv, and FastAPI! Now Live on fly.io endpoint"}
 
 
 
 @app.get("/health")
 def health_check():
-    return {"status"
-        : "healthy"}
+    return {"status" : "healthy"}
